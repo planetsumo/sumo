f9f8b69d30947e0770af454cc0d6c19b9eb61d4d
dkrajzew <dkrajzew@29f149b2-0c63-11e1-8f5c-a347f6a9acd0>
2013-04-19 11:48:14 +0000

    {dkrajzew} changes for inductive loop placement
    git-svn-id: http://svn.code.sf.net/p/sumo/code/branches/dkrajzew@13663 29f149b2-0c63-11e1-8f5c-a347f6a9acd0

--- tools/sumolib/geomhelper.py
+++ tools/sumolib/geomhelper.py
@@ -29,8 +29,7 @@ def minDistPositionPto2P(p, p1, p2, perpendicular=False):
     return l
   return u*l
 
-
-def minDistPositionPtoS(p, s):
+def minDistPositionPtoV(p, s):
   o = 0
   for i in range(0, len(s)-1):
     q = minDistPositionPto2P(p, s[i], s[i+1], True)
@@ -38,3 +37,25 @@ def minDistPositionPtoS(p, s):
       return o+q
     o = o + distance(s[i], s[i+1])
   return -1
+
+
+def distancePto2P(p, p1, p2, perpendicular=False):
+  l = distance(p1, p2)
+  u = (((p[0] - p1[0]) * (p2[0] - p1[0])) + ((p[1] - p1[1]) * (p2[1] - p1[1]))) / (l * l)
+  if u<0.0 or u>1:
+    if perpendicular:
+      return -1.
+    if u<0:
+      return distance(p1, p)
+    return distance(p2, p)
+  intersection = (p1[0] + u*(p2[0]-p1[0]), p1[1] + u*(p2[1]-p1[1]))
+  return distance(p, intersection)
+
+def distancePtoV(p, s):
+  for i in range(0, len(s)-1):
+    q = distancePto2P(p, s[i], s[i+1], True)
+    if q!=-1:
+      return q
+  return -1
+
+
