83e684dfd138dce09c75f5fc70b745c883514cae
dkrajzew <dkrajzew@29f149b2-0c63-11e1-8f5c-a347f6a9acd0>
2013-05-02 08:23:18 +0000

    merging with trunk
    git-svn-id: http://svn.code.sf.net/p/sumo/code/branches/dkrajzew@13813 29f149b2-0c63-11e1-8f5c-a347f6a9acd0

--- src/jtrrouter/jtrrouter_main.cpp
+++ src/jtrrouter/jtrrouter_main.cpp
@@ -147,19 +147,12 @@ computeRoutes(RONet& net, ROLoader& loader, OptionsCont& oc) {
     // initialise the loader
     loader.openRoutes(net);
     // prepare the output
-    net.openOutput(oc.getString("output-file"), false, oc.getString("vtype-output"));
+    net.openOutput(oc.getString("output-file"), "", oc.getString("vtype-output"));
     // build the router
     ROJTRRouter router(net, oc.getBool("ignore-errors"), oc.getBool("accept-all-destinations"),
                        (int)(((SUMOReal) net.getEdgeNo()) * OptionsCont::getOptions().getFloat("max-edges-factor")),
                        oc.getBool("ignore-vclasses"), oc.getBool("allow-loops"));
-    if (!oc.getBool("unsorted-input")) {
-        // the routes are sorted - process stepwise
-        loader.processRoutesStepWise(string2time(oc.getString("begin")), string2time(oc.getString("end")), net, router);
-    } else {
-        // the routes are not sorted: load all and process
-        loader.processAllRoutes(string2time(oc.getString("begin")), string2time(oc.getString("end")), net, router);
-    }
-    // end the processing
+    loader.processRoutes(string2time(oc.getString("begin")), string2time(oc.getString("end")), net, router);
     net.closeOutput();
 }
 
