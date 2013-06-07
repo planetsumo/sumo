c23c9c28983eeee032a74f16a7dcad86beea7e16
dkrajzew <dkrajzew@29f149b2-0c63-11e1-8f5c-a347f6a9acd0>
2013-06-07 07:13:54 +0000

    {colombo} adding a tls evaluation script; not yet embedded in tests
    git-svn-id: http://svn.code.sf.net/p/sumo/code/branches/colombo@14124 29f149b2-0c63-11e1-8f5c-a347f6a9acd0

--- tests/complex/traffic_lights/loop_flow/runner.py
+++ tests/complex/traffic_lights/loop_flow/runner.py
@@ -2,14 +2,25 @@
 # -*- coding: utf-8 -*-
 
 # call jtrrouter twice and check that the output differs
-import sys,os,subprocess,random
+import sys,os,subprocess,random,shutil
 sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), '..', '..', '..', '..', "tools"))
 from sumolib import checkBinary
 
-def buildFlows(simSteps, pWE, pEW, pNS, pSN):
-    fd = open("routes.rou.xml", "w")
-		#---routes---
-		print >> fd, """<routes>
+flow1def = "0;2400;200".split(";")
+flow2def = "0;2400;200".split(";")
+fillSteps = 3600#3600
+measureSteps = 3600#36000 
+simSteps = fillSteps + measureSteps 
+
+try: os.mkdir("results")
+except: pass
+try: os.mkdir("gfx")
+except: pass
+
+def buildDemand(simSteps, pWE, pEW, pNS, pSN):
+    fd = open("input_routes.rou.xml", "w")
+    #---routes---
+    print >> fd, """<routes>
 		
 			<vType id="type1" accel="2.0" decel="5.0" sigma="0.0" length="6.5" maxSpeed="70"/>
 		
@@ -19,87 +30,64 @@ def buildFlows(simSteps, pWE, pEW, pNS, pSN):
 			<route id="SN" edges="4i 2o 8o"/>
 			
 		"""
-		lastVeh = 0
-		vehNr = 0
-		for i in range(simSteps):
-			if random.uniform(0,1) < pWE: # Poisson distribution
-				print >> fd, '    <vehicle id="%i" type="type1" route="WE" depart="%i" departSpeed="13.89" />' % (vehNr, i)
-				vehNr += 1
-				lastVeh = i
-			if random.uniform(0,1) < pNS:
-				print >> fd, '    <vehicle id="%i" type="type1" route="NS" depart="%i" departSpeed="13.89" />' % (vehNr, i)
-				vehNr += 1
-				lastVeh = i
-			if random.uniform(0,1) < pEW:
-				print >> fd, '    <vehicle id="%i" type="type1" route="EW" depart="%i" departSpeed="13.89" />' % (vehNr, i)
-				vehNr += 1
-				lastVeh = i	
-			if random.uniform(0,1) < pSN:
-				print >> fd, '    <vehicle id="%i" type="type1" route="SN" depart="%i" departSpeed="13.89" />' % (vehNr, i)
-				vehNr += 1
-				lastVeh = i
-
-		print >> fd, "</routes>"
-		fd.close()  
+    lastVeh = 0
+    vehNr = 0
+    for i in range(simSteps):
+        if random.uniform(0,1) < pWE: # Poisson distribution
+            print >> fd, '    <vehicle id="%i" type="type1" route="WE" depart="%i" departSpeed="13.89" />' % (vehNr, i)
+            vehNr += 1
+            lastVeh = i
+        if random.uniform(0,1) < pNS:
+            print >> fd, '    <vehicle id="%i" type="type1" route="NS" depart="%i" departSpeed="13.89" />' % (vehNr, i)
+            vehNr += 1
+            lastVeh = i
+        if random.uniform(0,1) < pEW:
+            print >> fd, '    <vehicle id="%i" type="type1" route="EW" depart="%i" departSpeed="13.89" />' % (vehNr, i)
+            vehNr += 1
+            lastVeh = i	
+        if random.uniform(0,1) < pSN:
+            print >> fd, '    <vehicle id="%i" type="type1" route="SN" depart="%i" departSpeed="13.89" />' % (vehNr, i)
+            vehNr += 1
+            lastVeh = i
+    print >> fd, "</routes>"
+    fd.close()  
   
-def buildMeasures():
-  pass  
-  
+def patchTLSType(ifile, itype, ofile, otype):
+    fdi = open(ifile)
+    fdo = open(ofile, "w")
+    for line in fdi:
+        line = line.replace(itype, otype)
+        fdo.write(line)
+    fdo.close()
+    fdi.close()
+    
+      
 sumo = checkBinary('sumo')
 assert(sumo)
-args = [sumo,
-        '--net-file', 'net.net.xml',
-        '--route-files', 'routes.rou.xml',
-        '--tripinfo-output', 'tripinfos.xml',
-        ]
 
-flow1def = "0;2400;200".split(";")
-flow2def = "0;2400;200".split(";")
-steps = 100001
 
-measures = []
-
-for f1 in range(float(flow1def[0]), float(flow1def[1]), float(flow1def[2])):
-  	pWE = float(f1)/3600 # [veh/s]
+for f1 in range(int(flow1def[0]), int(flow1def[1]), int(flow1def[2])):
+    pWE = float(f1)/3600 # [veh/s]
     pEW = pWE
-    for f2 in range(float(flow2def[0]), float(flow2def[1]), float(flow2def[2])):
-    		pNS = float(f2)/3600 # [veh/s]
-    		pSN = pNS
-        buildRoutes(steps, pWE, pEW, pNS, pSN)
-        subprocess.call(args)
-        buildMeasures(f1, f2)
+    for f2 in range(int(flow2def[0]), int(flow2def[1]), int(flow2def[2])):
+        pNS = float(f2)/3600 # [veh/s]
+        pSN = pNS
+        print "Computing for %s<->%s" % (f1, f2)
+        buildDemand(simSteps, pWE, pEW, pNS, pSN)
+        for t in ["static", "agentbased", "actuated", "sotl_phase", "sotl_platoon", "sotl_request", "swarm"]:
+            print " for tls-type %s" % t
+            patchTLSType('input_additional_template.add.xml', '%tls_type%', 'input_additional.add.xml', t)
+            args = [sumo,
+              '--net-file', 'input_net.net.xml',
+              '--route-files', 'input_routes.rou.xml',
+              '--additional-files', 'input_additional.add.xml',
+              '--tripinfo-output', 'results/tripinfos_%s_%s_%s.xml' % (t, f1, f2),
+              '--summary-output', 'results/summary_%s_%s_%s.xml' % (t, f1, f2),
+              '--device.hbefa.probability', '1',
+              '--sloppy-insert'
+            ]
+            retCode = subprocess.call(args)
+            shutil.move("results/e2_output.xml", "results/e2_output_%s_%s_%s.xml" % (t, f1, f2))
+            shutil.move("results/e2_tl0_output.xml", "results/e2_tl0_output_%s_%s_%s.xml" % (t, f1, f2))
          
 
-
-def get_depart_lines(route_file):
-    return [l for l in open(route_file) if 'depart' in l]
-
-output_file1 = 'output1.rou.xml'
-output_file2 = 'output2.rou.xml'
-
-jtrrouter = checkBinary('jtrrouter')
-assert(jtrrouter)
-
-args = [jtrrouter,
-        '--net-file', 'input_net.net.xml',
-        '--flow-files', 'input_flows.flows.xml',
-        '--turn-ratio-files', 'input_turns.turns.xml',
-        '--output-file', output_file1,
-        '--sinks=end',
-        '--seed', None,
-        '--no-step-log',
-        '--randomize-flows']
-
-args[11] = str(random.randint(0, 2 ** 31))
-subprocess.call(args)
-route_lines1 = get_depart_lines(output_file1)
-
-args[8] = output_file2
-args[11] = str(random.randint(0, 2 ** 31))
-subprocess.call(args)
-route_lines2 = get_depart_lines(output_file2)
-
-if route_lines1 != route_lines2:
-    print('test passed. output is random')
-else:
-    print('test failed. output is deterministic')
