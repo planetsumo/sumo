ca4f9d0bc078be8f9cc7469f581eb0e981113a66
dkrajzew <dkrajzew@29f149b2-0c63-11e1-8f5c-a347f6a9acd0>
2013-11-08 07:18:01 +0000

    {vtd} merging with trunk
    git-svn-id: http://svn.code.sf.net/p/sumo/code/branches/vtd@15065 29f149b2-0c63-11e1-8f5c-a347f6a9acd0

--- docs/internet/daily/index.php
+++ docs/internet/daily/index.php
@@ -1,12 +1,38 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
-    <head>
-        <title>Status of daily build and test</title>
-    </head>
-    <body>
-        <center>
-            <h2>Status of daily build and test</h2>
-        </center>
+<!DOCTYPE html>
+
+<html lang="en" style="width: 100%">
+<head>
+  <title>SUMO - Status of daily build and tests</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="../sumo.css">
+  <link rel="license" href=
+  "http://creativecommons.org/licenses/by-sa/3.0/">
+</head>
+
+<body id="bmission" style="width: 100%">
+<div id="container" style="width: 100%">
+
+
+
+ <div id="logo" align="left"><img src="../logo.png" width="128" height="128" align="left"
+     alt="SUMO-Simulation of Urban Mobility" style="margin-left:10px;
+     margin-right:20px;"/>
+     <div id="logo_text1"> SUMO </div>
+     <div id="logo_text2"> Simulation of Urban MObility</div>
+ </div>
+
+ <div id="subsMenu"><ul>
+  <li class="sub"><a href="/">Home</a></li>
+  <li class="sub"><a href="/wiki/Downloads">Download</a></li>
+  <li class="sub"><a href="/userdoc/">Documentation</a></li>
+  <li class="sub"><a href="/wiki/">Wiki</a></li>
+  <li class="sub"><a href="/trac/">Trac</a></li>
+  <li class="sub"><a href="/blog/">Blog</a></li>
+  <li class="sub"><a href="/userdoc/Contact.html">Contact</a></li>
+  <li class="sub"><a href="http://sourceforge.net/projects/sumo/">SF-Project</a></li>
+ </ul></div>
+
+    <h2>Status of daily build and tests</h2>
         <table>
 <?PHP
 $column = 0;
@@ -65,6 +91,14 @@ for ($j = 0; $j < count($cells[0]); $j++) {
 }
 ?> 
         </table>
-    </body>
-</html>
+ </div>
 
+ <div id="footer">
+   <div>(c) 2011-2013, German Aerospace Center, Institute of Transportation Systems</div>
+   <div>Layout based on <a href="http://www.oswd.org/design/preview/id/3365">"Three Quarters"</a> by "SimplyGold"</div>
+ </div>
+                                                       
+ 
+</div></body>
+
+</html>
