f708876cb61887c0af5e43562fa84d7b6eb08176
rickybo89 <rickybo89@29f149b2-0c63-11e1-8f5c-a347f6a9acd0>
2014-07-07 13:39:36 +0000

    {COLOMBO} MSE2Collector: added a check when inserting vehicles in myKnownVehicles. If a vehicle's type is different than "COLOMBO_undetectable", then it's added, otherwise no. Used to simulate low penetration rate
    git-svn-id: http://svn.code.sf.net/p/sumo/code/branches/colombo@16727 29f149b2-0c63-11e1-8f5c-a347f6a9acd0

--- src/microsim/output/MSE2Collector.cpp
+++ src/microsim/output/MSE2Collector.cpp
@@ -88,6 +88,8 @@ MSE2Collector::notifyMove(SUMOVehicle& veh, SUMOReal oldPos,
     }
     if (newPos >= myStartPos && oldPos < myStartPos) {
         if (find(myKnownVehicles.begin(), myKnownVehicles.end(), &veh) == myKnownVehicles.end()) {
+			std::string type = veh.getVehicleType().getID(); // get vehicle's type
+			if(type.compare("COLOMBO_undetectable") != 0)
             myKnownVehicles.push_back(&veh);
         }
     }
@@ -123,6 +125,8 @@ MSE2Collector::notifyEnter(SUMOVehicle& veh, MSMoveReminder::Notification) {
     }
     if (veh.getPositionOnLane() >= myStartPos && veh.getPositionOnLane() - veh.getVehicleType().getLength() < myEndPos) {
         // vehicle is on detector
+		std::string type = veh.getVehicleType().getID(); // get vehicle's type
+		if(type.compare("COLOMBO_undetectable") != 0)
         myKnownVehicles.push_back(&veh);
         return true;
     }
