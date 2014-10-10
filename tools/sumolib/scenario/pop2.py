f890cff3338d33ea16629933775dcaf658cfa5d8
dkrajzew <dkrajzew@29f149b2-0c63-11e1-8f5c-a347f6a9acd0>
2014-10-10 07:48:25 +0000

    {colombo} some debugging (is not used currently)
    git-svn-id: http://svn.code.sf.net/p/sumo/code/branches/colombo@17180 29f149b2-0c63-11e1-8f5c-a347f6a9acd0

--- tools/sumolib/scenario/pop2.py
+++ tools/sumolib/scenario/pop2.py
@@ -1868,7 +1868,7 @@ class ScenarioSet_RiLSA1LoadCurvesOutTLS(ScenarioSet):
         if tlsID[0]=='e' or tlsID[0]=='w':
           tls._programs[prog]._phases[0][1] = self.params["other-green"]
         else:
-          tls._programs[prog]._phases[0][2] = self.params["other-green"]
+          tls._programs[prog]._phases[1][1] = self.params["other-green"]
         tls._programs[prog]._id = "adapted"
         fdo.write(tls._programs[prog].toXML(tlsID)+"\n")
     fdo.write("</additional>\n")
