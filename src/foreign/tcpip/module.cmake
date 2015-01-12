bae1d9f40307deeb8627b552f48a82a1a3517797
dkrajzew <dkrajzew@29f149b2-0c63-11e1-8f5c-a347f6a9acd0>
2015-01-12 12:11:26 +0000

    {colombo} removing external references as they seem to delay updates a lot
    git-svn-id: http://svn.code.sf.net/p/sumo/code/branches/colombo@17580 29f149b2-0c63-11e1-8f5c-a347f6a9acd0

---
+++ src/foreign/tcpip/module.cmake
@@ -0,0 +1,15 @@
+#=============
+# shawn module configuration for cmake build system
+#=============
+
+# Name of this module
+
+	set ( moduleName TCPIP )
+
+# Default status (ON/OFF)
+
+	set ( moduleStatus OFF )
+
+# List of libraries needed by this module, seperated by white space
+
+	set ( moduleLibs  )
